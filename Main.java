487a8a20687b96f9c49d4734f71f378eff30cffc
Elias Mimouni <emimouni10@hotmail.com>
2016-10-13 23:17:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,32 @@
 package com.company;
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        Square userInfo = new Square();
+        boolean b = true;
+        boolean c = true;
+        do {
+            double x = userInfo.perfectRandom();
+            System.out.println("What is your name?");
+            String s = input.next();
+            System.out.println("What is your next choice");
+            int y = input.nextInt();
+            do {
+                if (y == 0) {
+                    b=false;
+                    c=false;
+                }
+                if (y == 1) {
+                    System.out.println("Nice to meet you " + s + "! Your lucky number is " + (int) x);
+                    b=false;
+                    c=false;
+                }
+                if (y == 2) {
+                    break;
+                }
+            }while (b==true);
+        }while (c==true);
     }
 }
