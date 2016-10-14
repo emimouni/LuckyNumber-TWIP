52754393d1b2c4d5e8158ac4e01d79f068c6514e
Elias Mimouni <emimouni10@hotmail.com>
2016-10-13 23:18:18 -0400

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,8 +1,18 @@
 package com.company;
 
 /**
- * Created by em343 on 10/13/16.
+ * Created by emimo on 10/12/2016.
  */
 public class Square {
+    public int usersNumber;
 
+    public Square(){
+
+    }
+    public double perfectRandom(){
+        double i = Math.random()*1000;
+        usersNumber = (int)i;
+        usersNumber = (int)Math.pow(usersNumber,2);
+        return usersNumber;
+    }
 }
